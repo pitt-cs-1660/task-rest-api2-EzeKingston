f86c51352cc8929afbe78f72d8bacb12e240151f
EzeKingston <kingston.rody@gmail.com>
2025-02-02 07:18:20 -0500

    implement CRUD endpoints

--- cc_simple_server/server.py
+++ cc_simple_server/server.py
@@ -14,7 +14,8 @@ app = FastAPI()
 ############################################
 # Edit the code below this line
 ############################################
-
+# The database.py tells you the titles id,title,description,completed(boolean)
+# The models.py tells you what you need to return 
 
 @app.get("/")
 async def read_root():
@@ -33,11 +34,37 @@ async def create_task(task_data: TaskCreate):
     Args:
         task_data (TaskCreate): The task data to be created
 
+
     Returns:
         TaskRead: The created task data
     """
-    raise HTTPException(status_code=status.HTTP_501_NOT_IMPLEMENTED, detail="Not implemented")
+    #Task creation 
+    { "id": 42,
+     "title": "Attempt extra credit",
+     "description":"calculate time trial",
+     "completed":"false"}
+    #Args for task 
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute(
+    "INSERT INTO tasks (title, description, completed) VALUES (?, ?, ?)",
+    (task_data.title, task_data.description, task_data.completed),
+    )
+    
+    conn.commit()   
+    conn.close()
+   
+    post_instance = TaskRead(
+    id =42,
+     title= "Attempt extra credit",
+     description="calculate time trial",
+     completed="false"
+    )
 
+
+    return post_instance
+    
+   
 
 # GET ROUTE to get all tasks
 @app.get("/tasks/", response_model=list[TaskRead])
@@ -51,8 +78,25 @@ async def get_tasks():
     Returns:
         list[TaskRead]: A list of all tasks in the database
     """
-    raise HTTPException(status_code=status.HTTP_501_NOT_IMPLEMENTED, detail="Not implemented")
-
+     #Args for task 
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute(
+    "SELECT * FROM tasks"
+    )
+    rows = cursor.fetchall()
+    conn.commit() 
+        
+    conn.close()
+    
+    tasks=[]
+    
+    for r in rows:
+       tasks.append(TaskRead(id=r["id"], title=r["title"], description=r["description"], completed=bool(r["completed"])))
+    
+    return tasks
+    #return [TaskRead(id=r["id"], title=r["title"], description=r["description"], completed=bool(r["completed"])) for r in rows]
+    
 
 # UPDATE ROUTE data is sent in the body of the request and the task_id is in the URL
 @app.put("/tasks/{task_id}/", response_model=TaskRead)
@@ -67,7 +111,34 @@ async def update_task(task_id: int, task_data: TaskCreate):
     Returns:
         TaskRead: The updated task data
     """
-    raise HTTPException(status_code=status.HTTP_501_NOT_IMPLEMENTED, detail="Not implemented")
+
+     #Task update 
+    {"id":3,
+     "title": "finished extra credit",
+     "description":"expensive watch time trial",
+     "completed":"true"}
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute(
+    "UPDATE tasks SET title=?, description=?, completed=? WHERE id=?",
+    (task_data.title, task_data.description, task_data.completed, task_id),
+    )
+
+    #select all and loop through and find by task id and then update it. 
+    conn.commit()   
+    conn.close()
+
+    update_instance = TaskRead(id=3,
+     title= "finished extra credit",
+     description="expensive watch time trial",
+     completed="true"
+    )
+
+    return update_instance
+    #return TaskRead(id=3, title="finished extra credit", description="expensive watch time trial", completed="true")
+    #you don't want line 98. It will raise an error everytime 
+    #raise HTTPException(status_code=status.HTTP_501_NOT_IMPLEMENTED, detail="Not implemented")
+
 
 
 # DELETE ROUTE task_id is in the URL
@@ -82,4 +153,18 @@ async def delete_task(task_id: int):
     Returns:
         dict: A message indicating that the task was deleted successfully
     """
-    raise HTTPException(status_code=status.HTTP_501_NOT_IMPLEMENTED, detail="Not implemented")
+
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute(
+    "DELETE FROM tasks WHERE id=?", (task_id,)
+    )
+    conn.commit()   
+    
+    conn.close()
+
+    
+    return {"messege":f"Task {task_id} deleted successfully"}
+    #raise HTTPException(status_code=status.HTTP_501_NOT_IMPLEMENTED, detail="Not implemented")
+
+    
